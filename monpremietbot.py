d41bd564bddce3744d24f240c175471a39d7eeda
ballakeita <ballakei070@gmail.com>
2020-02-06 11:06:34 +0100

    monpremierbot

---
+++ monpremietbot.py
@@ -0,0 +1,14 @@
+#/usr/bin/python3
+#salutation = ["Salut","Bonjour","Bonsoir","Bonne journée","Bonne nuit","hello","hi","salam","saha","wellah"]
+question = input("Bonjour, je suis votre premier bot, veuillez donner un mot")
+
+
+if (question.lower() == "salut"):
+    print("Salut mon ami")
+elif (question.lower() == "bonjour"):
+    print("Salut mon ami")
+elif (question.lower() == "bonsoir"):
+    print("Salut mon ami")
+else:
+    print("Je ne comprend pas")
+
